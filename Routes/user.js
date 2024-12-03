2c645f7490d41f75246f13228970879e0e71cb21
muhammadaakhil <muhammadaakhil532@gmail.com>
2024-12-03 11:19:13 +0530

    signup page added

--- Routes/user.js
+++ Routes/user.js
@@ -1,10 +1,9 @@
 const express = require('express');
+const session = require('express-session');
 const { MongoClient } = require("mongodb");
 const uri = "mongodb://localhost:27017";
 const client = new MongoClient(uri);
 const user = express.Router();
-const username = "axxeff";
-const password = "7";
 
 
 user.get('/', (req, res) => {
@@ -12,7 +11,7 @@ user.get('/', (req, res) => {
         res.render('home');
     } else {
         if (req.session.passwordwrong) {
-            res.render('login', { message: 'invalid username or password' });
+            res.render('login', { message: 'invalid email or password' });
             req.session.passwordwrong = false
         } else {
             res.render('login');
@@ -21,16 +20,19 @@ user.get('/', (req, res) => {
 })
 
 
-user.post('/verify', (req, res) => {
+user.post('/verify', async (req, res) => {
     console.log(req.body);
-    if (req.body.username === username && req.body.password === password) {
+    const database = client.db('Secondloginpage');
+    const signup = database.collection('signup');
+    const wheresign = await signup.findOne({email:req.body.username,password:req.body.password})
+    // console.log(wheresign)
+    if (req.body.username === wheresign.email && req.body.password === wheresign.password) { 
         req.session.user = req.body.username;
         res.redirect('/home')
     } else {
         req.session.passwordwrong = true;
         res.redirect('/');
     }
-    const database = client.db('Secondloginpage');
     const user = database.collection('user');
     user.insertOne(req.body)
         .then(result => {
@@ -48,7 +50,7 @@ user.get('/home', (req, res) => {
         res.render('home')
     } else {
         if (req.session.passwordwrong) {
-            res.render('login', { message: 'invalid username or password' });
+            res.render('login', { message: 'invalid email or password' });
             req.session.passwordwrong = false
         } else {
             res.render('login')
@@ -74,7 +76,52 @@ user.post("/product", (req, res) => {
         .catch(error => {
             console.error('Error inserting document:', error);
         });
-    
+})
+
+user.post('/delete', (req, res) => {
+    res.status(200)
+    console.log(req.body)
+})
+
+
+
+user.get('/finddata', async (req, res) => {
+    try {
+        const database = client.db('Secondloginpage');
+        const product = database.collection('product');
+        const findresult = product.find({})
+        const cursor = await findresult.toArray();
+        res.json(cursor);
+        console.log(cursor);
+    } catch (error) {
+        console.log('Error finding data:', error);
+        res.status(500).send({ error: 'Failed to retrieve data' });
+    }
+});
+
+user.get('/findproduct', (req, res) => {
+    if(req.session.user){
+        res.render('finddata')
+    }else{
+        res.render('login')
+    }
+})
+
+user.get('/signup', (req, res) => {
+    res.render('signup'); 
+});
+
+user.post('/signaction', (req, res) => {
+    console.log(req.body)
+    const database = client.db('Secondloginpage');
+    const signup = database.collection('signup');
+    signup.insertOne(req.body)
+        .then(result => {
+            console.log('Document inserted:', result);
+        })
+        .catch(error => {
+            console.error('Error inserting document:', error);
+        });
 })
 
 
